b1709704e510f520946339e35b0250cc0f5f3ded
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-02-28 10:54:09 -0300

    criado a pasta Models e as classes Author e Book

---
+++ AudioVerseAPI/Models/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AudioVerse.Models;
+
+[Table("Author")]
+public class Author
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int YearBirth { get; set; }
+
+    public int YearDeath { get; set; }
+    
+    public string Biography { get; set; }
+
+    public bool Active { get; set; }
+}
---
+++ AudioVerseAPI/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AudioVerse.Models;
+
+[Table("Book")]
+public class Book 
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public int YearPublication { get; set; }
+
+    public string Synopsis { get; set; }
+
+    public string PdfLink { get; set; }
+
+    public string BookImage { get; set; }
+
+    public bool Active { get; set; }
+}
