2542ef8d23544552e195d524d4cc6bdee06722d8
nunes <nunesmath890@gmail.com>
2024-02-28 18:44:43 -0300

    adicionado o atributo Required nas classes Author e Book

--- AudioVerseAPI/Models/Author.cs
+++ AudioVerseAPI/Models/Author.cs
@@ -7,15 +7,24 @@ namespace AudioVerse.Models;
 [Table("Author")]
 public class Author
 {
+    [Key]
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Por favor, informe o nome do autor.")]
+    [StringLength(30, ErrorMessage = "O nome do autor deve possuir no máximo 30 caracteres.")]
     public string Name { get; set; }
 
+    [Required(ErrorMessage = "Por favor, informe o ano de nascimento do autor.")]
+    [StringLength(4, ErrorMessage = "O ano deve ter 4 caracteres")]
     public int YearBirth { get; set; }
 
+    [Required(ErrorMessage = "Por favor, informe o ano de falecimento do autor.")]
+    [StringLength(4, ErrorMessage = "O ano deve ter 4 caracteres")]
     public int YearDeath { get; set; }
-    
+
+    [Required(ErrorMessage = "Por favor, informe a biografia do autor.")]
     public string Biography { get; set; }
 
+    [Required(ErrorMessage = "Por favor, informe se o autor está ativo ou não.")]
     public bool Active { get; set; }
 }
--- AudioVerseAPI/Models/Book.cs
+++ AudioVerseAPI/Models/Book.cs
@@ -10,15 +10,23 @@ public class Book
     [Key]
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Por favor, digite o Título do livro.")]
+    [StringLength(40, ErrorMessage = "O nome do livro deve possuir no máximo 40 caracteres.")]
     public string Title { get; set; }
 
+    [Required(ErrorMessage = "Por favor, digite o ano de publicação do livro.")]
+    [StringLength(4, ErrorMessage = "O ano deve ter 4 caracteres")]
     public int YearPublication { get; set; }
 
+    [Required(ErrorMessage = "Por favor, digite a sinopse do livro.")]
     public string Synopsis { get; set; }
 
+    [Required(ErrorMessage = "Por favor, atribua um link de pdf para o livro.")]
     public string PdfLink { get; set; }
 
+    [Required(ErrorMessage = "Por favor, atribua uma url(imagem) do livro.")]
     public string BookImage { get; set; }
 
+    [Required(ErrorMessage = "Por favor, informe se o livro está ativo ou não.")]
     public bool Active { get; set; }
 }
