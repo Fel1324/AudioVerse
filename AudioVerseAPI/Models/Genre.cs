62087e0155514d0312baeaf49c0a12a8432a87ef
nunes <nunesmath890@gmail.com>
2024-02-28 19:57:12 -0300

    criada a classe Genre

---
+++ AudioVerseAPI/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AudioVerseAPI.Models;
+
+[Table("Genre")]
+public class Genre
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe o gênero.")]
+    public  string NameGenre { get; set; }
+}
