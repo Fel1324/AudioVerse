39a3836a5b5f4186747f77270c590748d2b12ff8
nunes <nunesmath890@gmail.com>
2024-03-21 20:56:37 -0300

    realizada algumas alterações no BookController

--- AudioVerseAPI/Controllers/BookController.cs
+++ AudioVerseAPI/Controllers/BookController.cs
@@ -10,11 +10,25 @@ public class BookController : ControllerBase
 {
 
     private static List<Book> books = new List<Book>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaBook([FromBody] Book book)
     {
+        book.Id = id++;
         books.Add(book);
         Console.WriteLine(book.Title);
+    }
+
+    [HttpGet]
+    public IEnumerable<Book> RecuperaBooks()
+    {
+        return books;
+    }
+
+    [HttpGet("{id}")]
+    public Book? RecuperaBookPorId (int id)
+    {
+        return books.FirstOrDefault(book => book.Id == id);
     }
 }
