32edb774a3dea8a52159ce22c03d45ed1eb5942e
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-04-23 21:05:57 -0300

    o componente Header foi estilizado

---
+++ AudioVerseReact/src/components/book/book.jsx
@@ -0,0 +1,5 @@
+export function Book(){
+  return (
+    <li></li>
+  )
+}
--- AudioVerseReact/src/components/header/Header.jsx
+++ AudioVerseReact/src/components/header/Header.jsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
+import { Search, Menu } from "lucide-react";
+
 import logo from "../../assets/logo.svg";
+import styles from "./Header.module.css";
 
 export function Header(){
   return (
-    <header>
-      <Link to="/">
-        <img src={logo} alt="Logo AudioVerse" />
-      </Link>
+    <header className={styles.header}>
+      <div className={styles.header__container}>
+        <Link to="/">
+          <img src={logo} alt="Logo AudioVerse" />
+        </Link>
+
+        <div>
+          <button className={`${styles.header__button} ${styles.openSearch}`} aria-label="Abrir barra de pesquisa">
+            <Search color="var(--icon-light-color)" strokeWidth={2.5} />
+          </button>
+
+          <button className={`${styles.header__button} ${styles.openMenu}`} aria-label="Abrir menu lateral">
+            <Menu color="var(--icon-light-color)" strokeWidth={2.5} />
+          </button>
+        </div>
+      </div>
     </header>
   )
 }
