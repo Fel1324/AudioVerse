07d43e25a250e1fd3f9d27687e29cd542edeb669
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-05-10 12:28:05 -0300

    começado a programar o banner da página Home

--- AudioVerseReact/src/pages/home/Home.jsx
+++ AudioVerseReact/src/pages/home/Home.jsx
@@ -1,13 +1,23 @@
 import { Header } from "../../components/header/Header.jsx";
 import { Footer } from "../../components/footer/Footer.jsx";
 
+import openBook from "../../assets/img/icons/openbook.svg";
+import styles from "./Home.module.css";
+
 export function Home(){
   return (
     <>
       <Header />
 
-      <main className="main">
-        <h1>Hello World!</h1>
+      <main className={`${styles.home} main`}>
+        <section className={styles.home__banner}>
+          <h1>O melhor da literatura nacional em forma de áudio</h1>
+          <img src={openBook} alt="" />
+        </section>
+
+        <div className={`${styles.home__container} container`}>
+
+        </div>
       </main>
 
       <Footer />
