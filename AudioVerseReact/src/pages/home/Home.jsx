308e636a3bb0bf9c8c684f97c56888014755eeb6
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-05-22 11:30:52 -0300

    criado botão de ouvir mais na página Home

--- AudioVerseReact/src/pages/home/Home.jsx
+++ AudioVerseReact/src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import { Book } from "../../components/book/Book.jsx";
 import { BookFilter } from "../../components/book-filter/BookFilter.jsx";
 import { Star } from "../../components/icons/Star.jsx";
 import { OpenBook } from "../../components/icons/OpenBook.jsx";
+import { HeadPhones } from "../../components/icons/HeadPhones.jsx";
 
 import styles from "./Home.module.css";
 
@@ -21,6 +22,38 @@ export function Home(){
     },
     {
       id: 2,
+      name: "Senhora",
+      bookCover: "https://ia800204.us.archive.org/32/items/LibrivoxCdCoverArt20/Senhora_1206.jpg",
+      author: "José de Alencar",
+      synopsis: "Senhora é um dos mais importantes clássicos da literatura brasileira, e o terceiro livro do mesmo autor que analisa a situação da mulher na sociedade brasileira do século XIX. Através da história de amor e separação entre Aurélia e Fernando, o autor critica a decadência da elite cortesã, a importância dada ao dinheiro e os casamentos por interesse. O romance, dividido em quatro partes, reflete este objetivo em sua própria estrutura, em que imita uma transação comercial: preço, quitação, posse, resgate.",
+      parentalRating: 15
+    },
+    {
+      id: 3,
+      name: "Senhora",
+      bookCover: "https://ia800204.us.archive.org/32/items/LibrivoxCdCoverArt20/Senhora_1206.jpg",
+      author: "José de Alencar",
+      synopsis: "Senhora é um dos mais importantes clássicos da literatura brasileira, e o terceiro livro do mesmo autor que analisa a situação da mulher na sociedade brasileira do século XIX. Através da história de amor e separação entre Aurélia e Fernando, o autor critica a decadência da elite cortesã, a importância dada ao dinheiro e os casamentos por interesse. O romance, dividido em quatro partes, reflete este objetivo em sua própria estrutura, em que imita uma transação comercial: preço, quitação, posse, resgate.",
+      parentalRating: 15
+    },
+    {
+      id: 4,
+      name: "Dom casmurro",
+      bookCover: "https://ia801802.us.archive.org/7/items/dom_casmurro_2102_librivox/domcasmurro_2102.jpg",
+      author: "Machado de Assis",
+      synopsis: "Dom Casmurro é um dos mais famosos romances escritos por Machado de Assis, publicado em 1899. Ele conta a história de Bento Santiago, o narrador da história que, como em uma autobiografia, se compõe de relatos desde sua mocidade até os dias em que está escrevendo o livro. Entre esses dois momentos, Bento escreve sobre suas reminiscências da juventude, sua vida no seminário, seu caso com Capitu e o ciúme desse relacionamento, que se torna o enredo central da trama. - Resumo escrito por Leni",
+      parentalRating: 15
+    },
+    {
+      id: 5,
+      name: "Dom casmurro",
+      bookCover: "https://ia801802.us.archive.org/7/items/dom_casmurro_2102_librivox/domcasmurro_2102.jpg",
+      author: "Machado de Assis",
+      synopsis: "Dom Casmurro é um dos mais famosos romances escritos por Machado de Assis, publicado em 1899. Ele conta a história de Bento Santiago, o narrador da história que, como em uma autobiografia, se compõe de relatos desde sua mocidade até os dias em que está escrevendo o livro. Entre esses dois momentos, Bento escreve sobre suas reminiscências da juventude, sua vida no seminário, seu caso com Capitu e o ciúme desse relacionamento, que se torna o enredo central da trama. - Resumo escrito por Leni",
+      parentalRating: 15
+    },
+    {
+      id: 6,
       name: "Senhora",
       bookCover: "https://ia800204.us.archive.org/32/items/LibrivoxCdCoverArt20/Senhora_1206.jpg",
       author: "José de Alencar",
@@ -75,6 +108,15 @@ export function Home(){
                   })
                 }
               </ul>
+
+              <button className={styles.audiobooks__listenMore}>
+                <div className={styles.listenMore__line} />
+                <span>
+                  Ouvir mais
+                  <HeadPhones />
+                </span>
+                <div className={styles.listenMore__line} />
+              </button>
             </div>
           </section>
         </div>
