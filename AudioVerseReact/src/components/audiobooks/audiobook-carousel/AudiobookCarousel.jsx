9248f62fbdf8ff5d87763c8fe108c2003d25f2a9
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-05-27 08:45:31 -0300

    terminado o componente AudiobookCarousel e finalizado a página Home com dados estáticos

--- AudioVerseReact/src/components/audiobooks/audiobook-carousel/AudiobookCarousel.jsx
+++ AudioVerseReact/src/components/audiobooks/audiobook-carousel/AudiobookCarousel.jsx
@@ -1,23 +1,48 @@
 import { ChevronLeft } from "../../icons/ChevronLeft.jsx";
 import { ChevronRight } from "../../icons/ChevronRight.jsx";
+import { Audiobook } from "../audiobook/Audiobook.jsx";
 
-export function AudiobookCarousel(){
-    return(
-        <section>
-            <div>
-                <div>
-                    <h2>Terminar</h2>
-                    <div>
-                        <button>
-                            <ChevronLeft />
-                        </button>
+import { audiobooks } from "../../../data/audiobooks.js";
+import styles from "./AudiobookCarousel.module.css"
+import { useEffect, useState } from "react";
 
-                        <button>
-                            <ChevronRight />
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+export function AudiobookCarousel() {
+  const [audiobook, setAudiobook] = useState([]);
+
+  useEffect(() => {
+    setAudiobook(audiobooks);
+  }, []);
+
+  return (
+    <section className={styles.audiobookCarousel}>
+      <div className={styles.audiobookCarousel__container}>
+        <div className={styles.audiobookCarousel__header}>
+          <h2 className={styles.audiobookCarousel__title}>Terminar</h2>
+          <div>
+            <button>
+              <ChevronLeft />
+            </button>
+
+            <button>
+              <ChevronRight />
+            </button>
+          </div>
+        </div>
+
+        <ul className={`${styles.AudiobookCarousel__list} display-grid`}>
+          {
+            audiobook.map((adbk) => {
+              return (
+                <Audiobook
+                  key={adbk.id}
+                  id={adbk.id}
+                  audiobookCover={adbk.audiobookCover}
+                />
+              )
+            })
+          }
+        </ul>
+      </div>
+    </section>
+  )
 }
--- AudioVerseReact/src/pages/home/Home.jsx
+++ AudioVerseReact/src/pages/home/Home.jsx
@@ -5,64 +5,13 @@ import { Footer } from "../../components/footer/Footer.jsx";
 import { Audiobook } from "../../components/audiobooks/audiobook/Audiobook.jsx";
 import { AudiobookFilter } from "../../components/audiobooks/audiobook-filter/AudiobookFilter.jsx";
 import { AudiobookCarousel } from "../../components/audiobooks/audiobook-carousel/AudiobookCarousel.jsx";
-import { Star } from "../../components/icons/Star.jsx";
 import { OpenBook } from "../../components/icons/OpenBook.jsx";
 import { HeadPhones } from "../../components/icons/HeadPhones.jsx";
 
+import { audiobooks } from "../../data/audiobooks.js";
 import styles from "./Home.module.css";
 
 export function Home(){
-  const audiobooks = [
-    {
-      id: 1,
-      name: "Dom casmurro",
-      audiobookCover: "https://ia801802.us.archive.org/7/items/dom_casmurro_2102_librivox/domcasmurro_2102.jpg",
-      author: "Machado de Assis",
-      synopsis: "Dom Casmurro é um dos mais famosos romances escritos por Machado de Assis, publicado em 1899. Ele conta a história de Bento Santiago, o narrador da história que, como em uma autobiografia, se compõe de relatos desde sua mocidade até os dias em que está escrevendo o livro. Entre esses dois momentos, Bento escreve sobre suas reminiscências da juventude, sua vida no seminário, seu caso com Capitu e o ciúme desse relacionamento, que se torna o enredo central da trama. - Resumo escrito por Leni",
-      parentalRating: 15
-    },
-    {
-      id: 2,
-      name: "Senhora",
-      audiobookCover: "https://ia800204.us.archive.org/32/items/LibrivoxCdCoverArt20/Senhora_1206.jpg",
-      author: "José de Alencar",
-      synopsis: "Senhora é um dos mais importantes clássicos da literatura brasileira, e o terceiro livro do mesmo autor que analisa a situação da mulher na sociedade brasileira do século XIX. Através da história de amor e separação entre Aurélia e Fernando, o autor critica a decadência da elite cortesã, a importância dada ao dinheiro e os casamentos por interesse. O romance, dividido em quatro partes, reflete este objetivo em sua própria estrutura, em que imita uma transação comercial: preço, quitação, posse, resgate.",
-      parentalRating: 15
-    },
-    {
-      id: 3,
-      name: "Senhora",
-      audiobookCover: "https://ia800204.us.archive.org/32/items/LibrivoxCdCoverArt20/Senhora_1206.jpg",
-      author: "José de Alencar",
-      synopsis: "Senhora é um dos mais importantes clássicos da literatura brasileira, e o terceiro livro do mesmo autor que analisa a situação da mulher na sociedade brasileira do século XIX. Através da história de amor e separação entre Aurélia e Fernando, o autor critica a decadência da elite cortesã, a importância dada ao dinheiro e os casamentos por interesse. O romance, dividido em quatro partes, reflete este objetivo em sua própria estrutura, em que imita uma transação comercial: preço, quitação, posse, resgate.",
-      parentalRating: 15
-    },
-    {
-      id: 4,
-      name: "Dom casmurro",
-      audiobookCover: "https://ia801802.us.archive.org/7/items/dom_casmurro_2102_librivox/domcasmurro_2102.jpg",
-      author: "Machado de Assis",
-      synopsis: "Dom Casmurro é um dos mais famosos romances escritos por Machado de Assis, publicado em 1899. Ele conta a história de Bento Santiago, o narrador da história que, como em uma autobiografia, se compõe de relatos desde sua mocidade até os dias em que está escrevendo o livro. Entre esses dois momentos, Bento escreve sobre suas reminiscências da juventude, sua vida no seminário, seu caso com Capitu e o ciúme desse relacionamento, que se torna o enredo central da trama. - Resumo escrito por Leni",
-      parentalRating: 15
-    },
-    {
-      id: 5,
-      name: "Dom casmurro",
-      audiobookCover: "https://ia801802.us.archive.org/7/items/dom_casmurro_2102_librivox/domcasmurro_2102.jpg",
-      author: "Machado de Assis",
-      synopsis: "Dom Casmurro é um dos mais famosos romances escritos por Machado de Assis, publicado em 1899. Ele conta a história de Bento Santiago, o narrador da história que, como em uma autobiografia, se compõe de relatos desde sua mocidade até os dias em que está escrevendo o livro. Entre esses dois momentos, Bento escreve sobre suas reminiscências da juventude, sua vida no seminário, seu caso com Capitu e o ciúme desse relacionamento, que se torna o enredo central da trama. - Resumo escrito por Leni",
-      parentalRating: 15
-    },
-    {
-      id: 6,
-      name: "Senhora",
-      audiobookCover: "https://ia800204.us.archive.org/32/items/LibrivoxCdCoverArt20/Senhora_1206.jpg",
-      author: "José de Alencar",
-      synopsis: "Senhora é um dos mais importantes clássicos da literatura brasileira, e o terceiro livro do mesmo autor que analisa a situação da mulher na sociedade brasileira do século XIX. Através da história de amor e separação entre Aurélia e Fernando, o autor critica a decadência da elite cortesã, a importância dada ao dinheiro e os casamentos por interesse. O romance, dividido em quatro partes, reflete este objetivo em sua própria estrutura, em que imita uma transação comercial: preço, quitação, posse, resgate.",
-      parentalRating: 15
-    }
-  ];
-
   const [audiobook, setAudiobook] = useState([]);
 
   useEffect(() => {
@@ -93,7 +42,7 @@ export function Home(){
             <div className={styles.audiobooks__container}>
               <h2>ÁudioBooks</h2>
 
-              <ul className={styles.audiobooks__list}>
+              <ul className={`${styles.audiobooks__list} display-grid`}>
                 {
                   audiobook.map((adbk) => {
                     return(
