a64dd88c3f9efce11b76bc466300d25d555a7686
Matheus-Bertolini <nunesmath890@gmail.com>
2024-05-29 08:16:40 -0300

    realizada a funcão de cadastras usuário

--- AudioVerseAPI/Controllers/UserController.cs
+++ AudioVerseAPI/Controllers/UserController.cs
@@ -1,18 +1,40 @@
 ﻿using AudioVerseAPI.Data.Dtos;
+using AudioVerseAPI.Models;
 
+using AutoMapper;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AudioVerseAPI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class UserController : ControllerBase
+public class UserController : ControllerBase 
 {
-    [HttpPost]
+    private IMapper _mapper;
+    private UserManager<User> _userManager;
 
-    public IActionResult RegisterUser
+    public UserController(IMapper mapper, UserManager<User> userManager)
+    {
+        _mapper = mapper;
+        _userManager = userManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterUser
         (CreateUserDto dto)
     {
-        throw new NotImplementedException();
+        User user = _mapper.Map<User>
+            (dto);
+
+        IdentityResult result = await _userManager.CreateAsync
+            (user, dto.Password);
+
+        if(result.Succeeded) return Ok("Usuário Cadastrado!");
+
+        throw new ApplicationException("Falha ao cadastrar: Sua senha deve conter letras, números e caracteres especiais");
     }
 }
+
+
