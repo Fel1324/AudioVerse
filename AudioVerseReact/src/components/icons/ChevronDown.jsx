4de5b6715bc22b0b0d440ac5e1f23a159513ef65
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-05-30 13:10:32 -0300

    começado a fazer configuração para tema claro/escuro

--- AudioVerseReact/src/components/icons/ChevronDown.jsx
+++ AudioVerseReact/src/components/icons/ChevronDown.jsx
@@ -1,7 +1,9 @@
-export function ChevronDown(){
-    return(
-        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M8 12L16 20L24 12" stroke="#69421D" strokeWidth="2" />
-        </svg>
-    )
+import "./styles.css";
+
+export function ChevronDown() {
+  return (
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path className="color-icon" d="M8 12L16 20L24 12" strokeWidth="2" />
+    </svg>
+  )
 }
