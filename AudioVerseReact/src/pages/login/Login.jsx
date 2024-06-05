0bc985eba6bd5f0a9a1316b1d0e98c6ed2b11105
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-06-05 11:09:39 -0300

    avançando no desenvolvimento da página de login

--- AudioVerseReact/src/pages/login/Login.jsx
+++ AudioVerseReact/src/pages/login/Login.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/logo.svg";
 import styles from "./Login.module.css";
 
 export function Login() {
+  const navigate = useNavigate();
+  
+  function navigateFromRegister(){
+    navigate("/register");
+  }
+
   return (
     <>
       <header className="secondary-header">
@@ -21,10 +28,10 @@ export function Login() {
             <h2>Formulário de login...</h2>
           </form>
 
-          <p className={`${styles.login__paragraph} paragraph`}>Não tem uma conta ainda?</p>
-          <Link to="/register">
-            Criar conta
-          </Link>
+          <div className={styles.login__footer}>
+            <p className={`${styles.login__paragraph} paragraph`}>Não tem uma conta ainda?</p>
+            <button className={styles.login__button} onClick={navigateFromRegister}>Criar conta</button>
+          </div>
         </div>
       </main>
     </>
