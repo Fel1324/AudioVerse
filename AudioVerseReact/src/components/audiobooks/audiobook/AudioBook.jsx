ddab0041cf2db4d59b4201d699ea7f3aa7ae3c73
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-06-12 12:26:22 -0300

    adicionado mais capas de audiobooks geradas por IA e estilizado o titulo dos audiobooks

--- AudioVerseReact/src/components/audiobooks/audiobook/AudioBook.jsx
+++ AudioVerseReact/src/components/audiobooks/audiobook/AudioBook.jsx
@@ -6,8 +6,8 @@ export function AudioBook({ id, name, audiobookCover, onOpenAudioBook }) {
   return (
     <li id={id} className={styles.audiobook}>
       <h3 className={styles.audiobook__title} title={name}>{name}</h3>
-      <img onClick={() => onOpenAudioBook(id)} src={audiobookCover} alt="" />
-
+      <img onClick={() => onOpenAudioBook(id)} src={audiobookCover} alt={``} />
+      
       <div>
         <BookMark />
         <span className={styles.audiobook__parentalRating}>15</span>
