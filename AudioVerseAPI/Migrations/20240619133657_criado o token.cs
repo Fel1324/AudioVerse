148d0ad888accae018f26aadd8f2bc5abe379885
Matheus-Bertolini <nunesmath890@gmail.com>
2024-06-19 10:45:36 -0300

    realizada apenas a migration

---
+++ AudioVerseAPI/Migrations/20240619133657_criado o token.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AudioVerseAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class criadootoken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
