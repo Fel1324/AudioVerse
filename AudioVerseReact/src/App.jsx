1b1897f9efc1b2d1dda0ee42704116b4363c6c30
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-07-22 09:29:09 -0300

    estrutura de rotas refatorada

--- AudioVerseReact/src/App.jsx
+++ AudioVerseReact/src/App.jsx
@@ -1,7 +1,33 @@
-import { Router } from "./Router.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Home } from "./pages/home/Home.jsx";
+import { AudioBookDetails } from "./pages/audio-book-details/AudioBookDetails.jsx";
+import { Login } from "./pages/login/Login.jsx";
+import { Register } from "./pages/register/Register.jsx";
 
 export function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/audiobook/:audioBookId",
+      element: <AudioBookDetails />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
-    <Router />
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
