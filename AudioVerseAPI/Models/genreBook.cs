359a10fc052a22a56dfb7db7392ea262652bf4a2
Matheus-Bertolini <nunesmath890@gmail.com>
2024-08-07 10:21:18 -0300

    criado a classe 'GenreBook' e código refatorado

--- AudioVerseAPI/Models/genreBook.cs
+++ AudioVerseAPI/Models/genreBook.cs
@@ -1,24 +1,6 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace AudioVerseAPI.Models;
-
-[Table("GenreBook")]
-public class GenreBook
+﻿namespace AudioVerseAPI.Models
 {
-    [Key]
-    public int Id { get; set; }
-
-    [Display(Name = "Nome do gênero")]
-    [Required(ErrorMessage = "Por favor, informe o gênero.")]
-    public int GenreId { get; set; }
-    [ForeignKey("GenreId")]
-    public Genre Genre { get; set; }
-
-    [Display(Name = "Livro")]
-    [Required(ErrorMessage = "Por favor, informe o nome do livro")]
-    public int BookId { get; set; }
-    [ForeignKey("BookId")]
-    public Book Book { get; set; }
+    public class GenreBook
+    {
+    }
 }
