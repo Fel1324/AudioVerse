87beef59608a0e6a8e7722bc8f61fb6cf4e4ec2e
Matheus-Bertolini <nunesmath890@gmail.com>
2024-08-23 11:49:42 -0300

    adicionada a classe 'Favoritos'

---
+++ AudioVerseAPI/Models/Favorito.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace AudioVerseAPI.Models;
+
+public class Favorito
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    public int BookId { get; set; }
+    [JsonIgnore]
+    public Book? Book { get; set; }
+
+    public int UserAppId { get; set; }
+    [JsonIgnore]
+    public UserApp? UserApp { get; set; }
+}
