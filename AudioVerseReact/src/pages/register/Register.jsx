4f58e28f8c7f75597f3ce42659d7f8cefe3d37a8
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2024-10-18 11:40:54 -0300

    mostrando erro quando o usuário já está cadastrado

--- AudioVerseReact/src/pages/register/Register.jsx
+++ AudioVerseReact/src/pages/register/Register.jsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 import { DefaultInput } from "../../components/forms/default-input/DefaultInput";
 import { PasswordInput } from "../../components/forms/password-input/PasswordInput";
+import { Message } from "../../components/layout/message/Message.jsx";
 
 import { api } from "../../lib/axios";
 import { useAuth } from "../../hooks/useAuth.js";
@@ -15,6 +16,8 @@ export function Register() {
   const { register, handleSubmit, watch, formState: {errors}} = useForm();
   const { isLoggedIn, setIsLoggedIn} = useAuth();
   const navigate = useNavigate();
+  const [message, setMessage] = useState();
+  const [messageText, setMessageText] = useState('');
 
   const password = watch('password');
 
@@ -37,8 +40,18 @@ export function Register() {
         .catch(error => console.error(error));
       }
     })
-    .catch(error => console.error(error));
+    .catch(error => {
+      setMessage(true);
+      setMessageText(error.response.data);
+    });
   }
+
+  setTimeout(() => {
+    if(message){
+      setMessage(false)
+    }
+    clearTimeout();
+  }, 3000)
 
   useEffect(() => {
     if(isLoggedIn){
@@ -47,78 +60,82 @@ export function Register() {
   }, [isLoggedIn]);
 
   return (
-    <div className="page-account">
-      <header className="secondary-header">
-        <Link to="/" className="logo">
-          <img className="secondary-logo" src={logo} alt="logo AudioVerse" />
-        </Link>
-      </header>
+    <>
+      {message && <Message text={messageText} />}
 
-      <main className="main">
-        <div className="secondary-container">
-          <p className="paragraph">Mergulhe em audiobooks de obras de domínio público, disponíveis para ouvir a qualquer momento, em qualquer lugar.</p>
-          <h1 className={styles.register__title}>Crie sua conta</h1>
-        
-          <form onSubmit={handleSubmit(createAccount)} className={styles.register__form} autoComplete="off">
-            <div className="form-container">
-              <div>
-                <DefaultInput
-                  error={errors?.username && true}
-                  content="Nome de usuário"
-                  {...register('username', {
-                    required: { message: 'O nome de usuário é obrigatório!', value: true },
-                    minLength: { message: 'O nome de usuário deve ter mais de 3 caracteres', value: 3},
-                    maxLength: { message: 'O nome de usuário precisa ter menos de 42 caracteres!', value: 42},
-                    pattern: {
-                      value: /^[A-Za-z0-9]+$/,
-                      message: 'O nome deve conter apenas letras e números, sem espaços e caracteres especiais!'
-                    },
-                  })}
-                />
-                {errors?.username?.message && <p className="error-message">{errors?.username?.message}</p>}
-              </div>
-              <div>
-                <PasswordInput
-                  error={errors?.password && true}
-                  content="Senha"
-                  {...register('password', {
-                    required: { message: 'A senha é obrigatória!', value: true },
-                    minLength: { message: 'A senha deve ter mais de 8 caracteres', value: 8},
-                    maxLength: { message: 'A senha deve ter menos de 32 caracteres', value: 32},
-                    pattern: {
-                      value: /^[A-Z][A-Za-z\d!@#$%^&*]*$/,
-                      message: 'A senha deve começar com uma letra maiúscula, conter números e apenas os caracteres especiais: !@#$%^&*'
-                    },
-                    validate: value => {
-                      const hasNumber = /\d/.test(value);
-                      const hasSpecialChar = /[!@#$%^&*]/.test(value);
-                      if (!hasNumber) return 'A senha deve conter pelo menos um número';
-                      if (!hasSpecialChar) return 'A senha deve conter pelo menos um caractere especial permitido (!@#$%^&*)';
-                      return true;
-                    },
-                  })}
-                />
-                {errors?.password?.message && <p className="error-message">{errors?.password?.message}</p>}
-              </div>
-              <div>
-                <PasswordInput
-                  error={errors?.confirmPassword && true}
-                  content="Confirme a Senha"
-                  {...register('confirmPassword', {
-                    required: { message: 'A confirmação da senha é obrigatória!', value: true },
-                    validate: value => value === password || 'As senhas não correspondem!'
-                  })}
-                />
-                {errors?.confirmPassword?.message && <p className="error-message">{errors?.confirmPassword?.message}</p>}
-              </div>
-            </div>
+      <div className="page-account">
+        <header className="secondary-header">
+          <Link to="/" className="logo">
+            <img className="secondary-logo" src={logo} alt="logo AudioVerse" />
+          </Link>
+        </header>
 
-            <button className={`submit ${styles.register__submit}`} type="submit">
-              Criar
-            </button>
-          </form>
-        </div>
-      </main>
-    </div>
+        <main className="main">
+          <div className="secondary-container">
+            <p className="paragraph">Mergulhe em audiobooks de obras de domínio público, disponíveis para ouvir a qualquer momento, em qualquer lugar.</p>
+            <h1 className={styles.register__title}>Crie sua conta</h1>
+          
+            <form onSubmit={handleSubmit(createAccount)} className={styles.register__form} autoComplete="off">
+              <div className="form-container">
+                <div>
+                  <DefaultInput
+                    error={errors?.username && true}
+                    content="Nome de usuário"
+                    {...register('username', {
+                      required: { message: 'O nome de usuário é obrigatório!', value: true },
+                      minLength: { message: 'O nome de usuário deve ter mais de 3 caracteres', value: 3},
+                      maxLength: { message: 'O nome de usuário precisa ter menos de 42 caracteres!', value: 42},
+                      pattern: {
+                        value: /^[A-Za-z0-9]+$/,
+                        message: 'O nome deve conter apenas letras e números, sem espaços e caracteres especiais!'
+                      },
+                    })}
+                  />
+                  {errors?.username?.message && <p className="error-message">{errors?.username?.message}</p>}
+                </div>
+                <div>
+                  <PasswordInput
+                    error={errors?.password && true}
+                    content="Senha"
+                    {...register('password', {
+                      required: { message: 'A senha é obrigatória!', value: true },
+                      minLength: { message: 'A senha deve ter mais de 8 caracteres', value: 8},
+                      maxLength: { message: 'A senha deve ter menos de 32 caracteres', value: 32},
+                      pattern: {
+                        value: /^[A-Z][A-Za-z\d!@#$%^&*]*$/,
+                        message: 'A senha deve começar com uma letra maiúscula, conter números e apenas os caracteres especiais: !@#$%^&*'
+                      },
+                      validate: value => {
+                        const hasNumber = /\d/.test(value);
+                        const hasSpecialChar = /[!@#$%^&*]/.test(value);
+                        if (!hasNumber) return 'A senha deve conter pelo menos um número';
+                        if (!hasSpecialChar) return 'A senha deve conter pelo menos um caractere especial permitido (!@#$%^&*)';
+                        return true;
+                      },
+                    })}
+                  />
+                  {errors?.password?.message && <p className="error-message">{errors?.password?.message}</p>}
+                </div>
+                <div>
+                  <PasswordInput
+                    error={errors?.confirmPassword && true}
+                    content="Confirme a Senha"
+                    {...register('confirmPassword', {
+                      required: { message: 'A confirmação da senha é obrigatória!', value: true },
+                      validate: value => value === password || 'As senhas não correspondem!'
+                    })}
+                  />
+                  {errors?.confirmPassword?.message && <p className="error-message">{errors?.confirmPassword?.message}</p>}
+                </div>
+              </div>
+
+              <button className={`submit ${styles.register__submit}`} type="submit">
+                Criar
+              </button>
+            </form>
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
