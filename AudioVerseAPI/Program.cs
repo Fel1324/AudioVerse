086c5c74584f65d43029a6dd7db1950bd8d7247a
Matheus-Bertolini <nunesmath890@gmail.com>
2024-11-16 13:36:33 -0300

    atualizado o program

--- AudioVerseAPI/Program.cs
+++ AudioVerseAPI/Program.cs
@@ -13,6 +13,100 @@ var applicationName = "AudioVerse";
 
 var builder = WebApplication.CreateBuilder(args);
 
+/*CÓDIGO DO AUTHORIZATION NO SWAGGER
+var builder = WebApplication.CreateBuilder(args);
+
+// Outras configurações do builder...
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "AudioVerseAPI",
+        Version = "v1"
+    });
+
+    // Configuração para o JWT
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Insira o token JWT no formato: Bearer {seu token}"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+// Outras configurações do builder...
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: applicationName,
+        policy =>
+        {
+            policy.WithOrigins("*").
+                WithHeaders("content-type").
+                AllowAnyMethod();
+        });
+});
+
+var connectionString = builder.Configuration.GetConnectionString("AudioVerseConnection");
+
+builder.Services.AddDbContext<AudioVerseAPI.Data.AudioVerseContext>(opts =>
+    opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services
+    .AddIdentity<UserApp, IdentityRole>()
+    .AddEntityFrameworkStores<AudioVerseContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// Add services to the container.
+
+builder.Services.AddScoped<UserAppService>();
+builder.Services.AddScoped<TokenService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes
+            ("9ASHDA98H9ah9ha9H9A89n0fjhkjsjafkjhakhfhal8e789798qy23yhuiyhyhuya")),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+    };
+}); 
+*/
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: applicationName,
